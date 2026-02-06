e4d03345385de69eac230195e25b2643bcd6cefe
fekie <fekie@proton.me>
2026-02-06 02:01:47 -0500

    chore: add meowbox

--- src/states/light_ring.rs
+++ src/states/light_ring.rs
@@ -1,13 +1,21 @@
-use super::State;
+use super::{Meowbox, State};
+use crate::tasks::all_leds_off;
 
 // Light Ring
-impl State {
-    pub(super) fn control_light_ring(&mut self) {
-        // choose which method to run based on which function is
-        // currently running
+impl Meowbox {
+    pub(super) async fn setup_light_ring(&mut self) {}
+
+    pub(super) async fn execute_light_ring(&mut self) {}
+
+    /// This method is called if the state is in shutdown. Shutdown
+    /// is only started when an item exists in next_state.
+    pub(super) async fn shutdown_light_ring(&mut self) {
+        // TODO: turn all lights off
+        all_leds_off().await;
+
+        self.state = self.next_state.take().unwrap();
+
+        // change to next state
+        //*self = self.next_state;
     }
-
-    fn setup_light_ring(&mut self) {}
-
-    fn execute_light_ring(&mut self) {}
 }
--- src/states/mod.rs
+++ src/states/mod.rs
@@ -14,6 +14,89 @@ use super::hardware::{
 pub mod error_state;
 pub mod light_ring;
 
+/// State must be contained inside a wrapper. This is because
+/// including next_state within State would cause an infinite size
+/// type. Because a Box requires a heap (which the program does not
+/// have), a wrapper containing both must be used.
+pub struct Meowbox {
+    state: State,
+    /// If next_state exists, the next tick will go into shutdown.
+    /// Each tick, the shutdown function will be run until the
+    /// shutdown function changes the state and clears
+    /// next_state.
+    next_state: Option<State>,
+    /// This is set to true when a shutdown is needed. When this is
+    /// set to true, the state will go into shutdown mode, but
+    /// `next_state` will not be set to None until the state is
+    /// actually transisitoned.
+    needs_to_shutdown: bool,
+}
+
+impl Meowbox {
+    pub async fn tick(&mut self) {
+        self.check_for_shutdown_transition();
+
+        match self.state {
+            State::LightRing(stage, light_ring_state) => {
+                match stage {
+                    Stage::Setup => self.setup_light_ring().await,
+                    Stage::Execution => {
+                        self.execute_light_ring().await
+                    }
+                    // shutdown should
+                    Stage::Shutdown => {
+                        self.shutdown_light_ring().await
+                    }
+                }
+            }
+            State::ErrorState(etype) => match etype {
+                _ => {
+                    RED_LED.lock().await.as_mut().unwrap().toggle();
+                    error!("error state");
+                    Timer::after(Duration::from_millis(200)).await;
+                }
+            },
+
+            // If we dont yet have the state implemented, go to the
+            // error state.
+            _ => {
+                self.state = State::ErrorState(
+                    ErrorStateType::StateNotImplemented,
+                )
+            }
+        }
+    }
+
+    /// Goes into shutdown if `needs_to_shutdown` is `true`
+    fn check_for_shutdown_transition(&mut self) {
+        if !self.needs_to_shutdown {
+            return;
+        }
+
+        // If there is a need to shutdown, then set to shutdown.
+        // Annoying, a match tree has to be used here.
+        match self.state {
+            State::Menu(_) => {
+                self.state = State::Menu(Stage::Shutdown);
+            }
+            State::LightRing(_, light_ring_state) => {
+                self.state = State::LightRing(
+                    Stage::Shutdown,
+                    light_ring_state,
+                );
+            }
+            State::FlowField(_, flow_field_state) => {
+                self.state = State::FlowField(
+                    Stage::Shutdown,
+                    flow_field_state,
+                )
+            }
+            // If we hit errorr state, dont change anything
+            State::ErrorState(_) => {}
+        }
+    }
+}
+
 /// The overarching state of the machine that specifies which routines
 /// it is currently running. States can contain smaller substates.
 #[derive(Clone, Copy, Debug)]
@@ -25,31 +108,9 @@ pub enum State {
 }
 
 impl State {
-    /// Run the next tick based on the current state.
-    pub async fn tick(&mut self) {
-        match self {
-            Self::LightRing(_, _) => {
-                self.control_light_ring();
-            }
-            Self::ErrorState(etype) => match etype {
-                _ => {
-                    RED_LED.lock().await.as_mut().unwrap().toggle();
-                    error!("error state");
-                    Timer::after(Duration::from_millis(200)).await;
-                }
-            },
-
-            // If we dont yet have the state implemented, go to the
-            // error state.
-            _ => {
-                *self = Self::ErrorState(
-                    ErrorStateType::StateNotImplemented,
-                )
-            }
-        }
-    }
-
-    pub fn setup() {}
+    // Run the next tick based on the current state. Optionally
+    // return the next state to switch to.
+    //async fn tick(&mut self) -> Option<State> {}
 }
 
 /// Used to determine which stage of the command/state is currently
